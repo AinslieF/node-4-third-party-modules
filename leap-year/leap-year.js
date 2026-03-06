0984e76a0127019d4de3181a7547c098d6dfd5e0
AinslieF <ainsliefrancisdec16@gmail.com>
2026-03-06 12:10:29 -0500

    Leap year js

--- leap-year/leap-year.js
+++ leap-year/leap-year.js
@@ -5,3 +5,16 @@ Make an app that determines if a given year is a leap year!
 
 Read the instructions in the README.md file in this folder.
 ------------------------------ */
+// import the moment node module
+import moment from "moment";
+
+ // get user input
+let year = Number(process.argv[2]);
+
+// determines if the input year is a leap year or not
+
+if (moment([year]).isLeapYear() === true) {
+    console.log(`${year} is a leap year! Phil get's a birthday!`)
+} else {
+    console.log(`${year} is not a leap year. No birthday for Phil.`);
+}
